c336bcd1f5aa4f4345b98378da81d7269b7f084a
Alan Escobedo <ingescobedoalan4@gmail.com>
2023-03-18 13:08:00 -0600

    Archivos y estructura de carpetas + assets

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,20 @@
+import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
+import { Navbar } from './components'
+import './index.css'
+import './App.scss';
+
 const App = () => {
   return (
-    <p>
-      Click on the Vite and React logos to learn more
-    </p>
+    <div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
+    </div>
   );
-}
+};
 
 export default App;
---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import './Navbar.scss'
+
+
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        {/* <img src={images.logo} alt="Logo" /> */}
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar

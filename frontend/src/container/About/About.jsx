2c28f9e66ae93b608b6b3517aacefdce6b61f9dc
Alan Escobedo <ingescobedoalan4@gmail.com>
2023-03-18 18:02:44 -0600

    Conexion con sanity para mostrar las tarjetas sobre mi

--- frontend/src/container/About/About.jsx
+++ frontend/src/container/About/About.jsx
@@ -1,9 +1,50 @@
-import './About.scss'
+import { motion } from "framer-motion";
+import { useState, useEffect } from "react";
+
+import { images } from "../../constants";
+import "./About.scss";
+import { urlFor, client } from '../../client'
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+  const [abouts, setAbouts] = useState([]);
 
-export default About
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query)
+      .then((data) => setAbouts(data));
+
+    console.log(abouts)
+  }, []);
+  return (
+    <>
+      <h2 className="head-text">
+        I Know that<span> Good Dev</span>
+        <br />
+        means<span> Good Apps</span>
+      </h2>
+
+      <div className="app__profiles">
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: "tween" }}
+            className="app__profile-item"
+            key={about.title + index}
+          >
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <h2 className="p-text" style={{ marginTop: 10 }}>
+              {about.description}
+            </h2>
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default About;

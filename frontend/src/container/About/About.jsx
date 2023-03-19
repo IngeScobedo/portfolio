9eb7c07981c589983a467bef141313137791e1f3
Alan Escobedo <ingescobedoalan4@gmail.com>
2023-03-18 18:31:08 -0600

    Se creo un componente de orden superior <AppWrap /> para tener los botones de redes sociales y los puntos de navegacion en cada seccion

--- frontend/src/container/About/About.jsx
+++ frontend/src/container/About/About.jsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion";
 import { useState, useEffect } from "react";
 
-import { images } from "../../constants";
+import { AppWrap } from '../../wrapper'
 import "./About.scss";
 import { urlFor, client } from '../../client'
 
@@ -47,4 +47,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default AppWrap(About, 'about');
